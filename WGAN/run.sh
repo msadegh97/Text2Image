354e3e2816bf46264c269684445048929f2a43aa
msadegh97 <ms.akhondzadeh@gmail.com>
2021-06-29 17:04:04 +0430

    edit script run for flowers dataset

--- WGAN/run.sh
+++ WGAN/run.sh
@@ -2,4 +2,7 @@
 
 cd /home/hlcv_team019/Text2Image/
 wandb login 7a44e6f35f9bf51e15cefc85c9c65093fc9c5d87
-/home/hlcv_team019/miniconda3/envs/myenv1/bin/python train.py --log_interval 50 --ngpu 1 --dataset birds --num_workers 10
+
+/home/hlcv_team019/miniconda3/envs/myenv1/bin/python train.py --log_interval 50 --ngpu 5 --dataset birds --num_workers 10
+
+/home/hlcv_team019/miniconda3/envs/myenv1/bin/python train.py --log_interval 50 --ngpu 5 --dataset flowers --num_workers 10
