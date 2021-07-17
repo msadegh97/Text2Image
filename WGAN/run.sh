451ca64cb4e91c90792b5b3ac36e695d097652a0
msadegh97 <ms.akhondzadeh@gmail.com>
2021-07-17 20:16:02 +0200

    bug fix wgan

--- WGAN/run.sh
+++ WGAN/run.sh
@@ -3,9 +3,9 @@
 cd /home/hlcv_team019/Text2Image/WGAN/
 wandb login 7a44e6f35f9bf51e15cefc85c9c65093fc9c5d87
 
-/home/hlcv_team019/miniconda3/envs/myenv1/bin/python train.py --experiment_name "WGAN_BIRDS_CLS2" --num_epochs 300 --log_interval 100 --lr 0.00005 --ngpu 1 --dataset birds --num_workers 8 --wandb=True --cls=True
+/home/hlcv_team019/miniconda3/envs/myenv1/bin/python train.py --experiment_name "WGAN_BIRDS_CLS_emb" --num_epochs 601 --log_interval 200 --lr 0.0001 --ngpu 1 --dataset birds --num_workers 8 --wandb=True --cls=True
 
-/home/hlcv_team019/miniconda3/envs/myenv1/bin/python train.py --experiment_name "WGAN_FLOWERS_CLS2" --num_epochs 300 --lr 0.00005 --log_interval 100 --ngpu 1 --dataset flowers --num_workers 8 --wandb=True --cls=True
+#/home/hlcv_team019/miniconda3/envs/myenv1/bin/python train.py --experiment_name "WGAN_FLOWERS_CLS2" --num_epochs 300 --lr 0.00005 --log_interval 100 --ngpu 1 --dataset flowers --num_workers 8 --wandb=True --cls=True
 
 #/home/hlcv_team019/miniconda3/envs/myenv1/bin/python train.py --experiment_name "WGAN_BIRDS2" --num_epochs 300 --lr 0.0001 --log_interval 100 --ngpu 1 --dataset birds --num_workers 8 --wandb=True --cls=False
 
